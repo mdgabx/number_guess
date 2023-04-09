cd959c7c06f1eaa570818c10473801c4b403ef02
Mark Dexter <a.markdexter@gmail.com>
2023-04-09 20:35:05 +0800

    Update number_guess.sql

--- number_guess.sql
+++ number_guess.sql
@@ -16,17 +16,17 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE user_db;
+DROP DATABASE number_guess;
 --
--- Name: user_db; Type: DATABASE; Schema: -; Owner: freecodecamp
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
 --
 
-CREATE DATABASE user_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
 
 
-ALTER DATABASE user_db OWNER TO freecodecamp;
+ALTER DATABASE number_guess OWNER TO freecodecamp;
 
-\connect user_db
+\connect number_guess
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
