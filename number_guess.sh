1e0b00625f0da457f5ab70e9d142b9d06469c7a1
Mark Dexter <a.markdexter@gmail.com>
2023-04-11 12:57:30 +0800

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -8,20 +8,65 @@ PSQL="psql -X --username=freecodecamp --dbname=number_guess -t --no-align -c"
 # randomly generate number from 1 to 1000
 
 N=$(( RANDOM % 1000 )) # secret number
+echo $N
 
 
-echo -e "\nEnter your username:"
+echo "Enter your username:"
 read USERNAME
 
-VIEW_USER=$($PSQL "SELECT * FROM players WHERE username='$USERNAME';")
+GAME_LOGIC() {
+  while true;
+  do
+    (( COUNTER++ ))
+    if (( $GUESS > $N )) 
+    then 
+      echo -e "\nIt's lower than that, guess again:"
+    elif (( $GUESS < $N ))
+    then
+      echo -e "\nIt's higher than that, guess again: "
+    else 
+      echo -e "\nYou guessed it in $COUNTER tries. The secret number was $N. Nice job!" 
+      break
+    fi
+
+    read GUESS
+  done
+
+  # update player stats
+  UPDATE_STATS=$($PSQL "UPDATE players SET games_played=games_played+1, best_game=LEAST(best_game, $COUNTER) WHERE username='$USERNAME';")
+
+}
+
+
+
+VIEW_USER=$($PSQL "SELECT username, games_played, best_game FROM players WHERE username='$USERNAME';")
+
+COUNTER=0
 
 if [[ -z $VIEW_USER ]]
 then
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 
   # insert the player username
-  INSERT_USERNAME=$($PSQL "INSERT INTO players(username, games_played, best_game) VALUES('$USERNAME', 1, 0);")
+  INSERT_USERNAME=$($PSQL "INSERT INTO players(username, games_played) VALUES('$USERNAME' , 0);")
+
+  echo -e "\nGuess the secret number between 1 and 1000:"
+  read GUESS
+
+  GAME_LOGIC
 
 else 
-  echo "existing user"
+  
+  echo "$VIEW_USER" | while IFS='|' read -r USERNAME GAMES_PLAYED BEST_GAME
+  do
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+
+  echo -e "\nGuess the secret number between 1 and 1000:"
+  read GUESS
+
+  GAME_LOGIC
+
+ 
 fi
+
